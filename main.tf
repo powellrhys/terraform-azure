cfd3f1a84ee3a8f7c454da174fe3bca543e9d0f3
Rhys Powell <55314665+powellrhys@users.noreply.github.com>
2024-05-10 10:08:11 +0100

    train-delay app added to deployment (#8)
    * train-delay app added to deployment
    
    * env variables added to train delay app
    
    * train app variables added to variables.tf

--- main.tf
+++ main.tf
@@ -64,6 +64,23 @@ module "tf-number-classification" {
   }
 }
 
+module "train-delay" {
+  source              = "./modules/webapp"
+  web_app_name        = "train-delay"
+  location            = azurerm_resource_group.webapp_rg.location
+  resource_group_name = azurerm_resource_group.webapp_rg.name
+  service_plan_id     = azurerm_service_plan.webapp_service_plan.id
+  docker_image        = "powellrhys/train-delay"
+  docker_image_tag    = "latest"
+  app_settings = {
+    DOCKER_REGISTRY_SERVER_URL = "https://index.docker.io/v1"
+    PORT                       = "8501"
+    WEBSITES_PORT              = "8501"
+    account_username           = var.train_delay_username
+    account_password           = var.train_delay_password
+  }
+}
+
 resource "azurerm_resource_group" "sql_rg" {
   name     = "sql-resource-group"
   location = "West Europe"
